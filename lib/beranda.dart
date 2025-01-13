30d88051d4a90b158d692aa3a81a690090db13b8
sallimar <sallimarxpplg1@gmai.com>
2025-01-13 15:32:59 +0700

    kedua

--- lib/beranda.dart
+++ lib/beranda.dart
@@ -1,1 +1,37 @@
 import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const Beranda());
+}
+
+class Beranda extends StatefulWidget {
+  const Beranda({Key? key}) : super(key: key);
+
+  @override
+  _BerandaState createState() => _BerandaState();
+}
+
+class _BerandaState extends State<Beranda> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Beranda',
+          style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          leading: IconButton(onPressed: () {
+            Navigator.pop(context);
+          }, icon: const Icon(Icons.arrow_back),
+          color: Colors.white,),
+        ),
+        body: Center(
+          child: const Text("selamat datang di beranda!!!"),
+        ),
+      ),
+    );
+  }
+}
